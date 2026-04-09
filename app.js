6d628a31b1dfc9bf1ab5d09ff932263cacec3d45
Junior Antunes <jugabriel2612@gmail.com>
2026-04-09 14:08:19 -0300

    Feat: Conexão com banco de dados via prisma

--- app.js
+++ app.js
@@ -1,88 +1,75 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
-const methodOverride = require('method-override')
-app.use(methodOverride('_method'))
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 
-app.set('view engine', 'ejs')//Config ejs
+app.set("view engine", "ejs"); //Config ejs
 
 let produtos = [
-    {id: 1, nome: "prod1", preco: 144 },
-    {id: 2, nome: "prod2", preco: 1334 }
-]
+  { id: 1, nome: "prod1", preco: 144 },
+  { id: 2, nome: "prod2", preco: 1334 },
+];
 
-
-
-
-app.get('/', (req, res) => {
-    res.render('index', { listaProdutos: produtos})
+app.get("/", (req, res) => {
+  res.render("index", { listaProdutos: produtos });
 });
 
-app.get('/cadastrar', (req, res) => {
-    res.render('formCadastro', {})
-})
+app.get("/cadastrar", (req, res) => {
+  res.render("formCadastro", {});
+});
 
-app.use(express.urlencoded({extended: true})); // Recebe os dados do formulario pq senao chega tudo com undefined 
+app.use(express.urlencoded({ extended: true })); // Recebe os dados do formulario pq senao chega tudo com undefined
 
+app.post("/produtos", (req, res) => {
+  const { nome, preco } = req.body;
 
-app.post('/produtos', (req, res) => {
-    
-    const { nome, preco } = req.body;
+  const novoProduto = {
+    id: Date.now(),
+    nome: nome,
+    preco: parseFloat(preco),
+  };
 
-    const novoProduto = {
-        id: Date.now(),
-        nome: nome,
-        preco: parseFloat(preco)
+  produtos.push(novoProduto);
+
+  res.redirect("/");
+});
+
+app.listen(PORT, () => {
+  console.log("Rodando");
+});
+
+app.delete("/produtos/:id", (req, res) => {
+  const idDelete = parseInt(req.params.id);
+  produtos = produtos.filter((p) => p.id != idDelete);
+
+  res.redirect("/");
+});
+
+app.get("/editar/:id", (req, res) => {
+  const idEdita = parseInt(req.params.id);
+  produto = produtos.find((p) => p.id === idEdita);
+  if (produto) {
+    res.render("editar", { produto: produto });
+  } else {
+    res.status(404).send("Produto nao foi encontrado");
+  }
+});
+
+app.put("/produtos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { nome, preco } = req.body;
+
+  const index = produtos.findIndex((p) => p.id === id);
+
+  if (index !== -1) {
+    produtos[index] = {
+      id: id,
+      nome: nome,
+      preco: parseFloat(preco),
     };
-
-    produtos.push(novoProduto);
-
-    res.redirect('/');
-
-
-
-})
-
-app.listen(PORT, () =>{
-    console.log("Rodando");
-    
-})
-
-
-
-app.delete('/produtos/:id', (req, res) => {
-    const idDelete = parseInt(req.params.id);
-    produtos = produtos.filter(p => p.id != idDelete);
-
-    res.redirect('/')
-})
-
-
-app.get('/editar/:id', (req, res) => {
-    const idEdita = parseInt(req.params.id);
-    produto = produtos.find(p => p.id === idEdita);
-    if(produto){
-        res.render('editar', {produto: produto})
-    }else{
-        res.status(404).send("Produto nao foi encontrado");
-    }
-})
-
-app.put('/produtos/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const { nome, preco} = req.body;
-
-    const index = produtos.findIndex(p => p.id === id)
-
-    if(index !== -1){
-        produtos[index] = {
-            id: id,
-            nome: nome,
-            preco: parseFloat(preco)
-        }
-        res.redirect('/')
-    }else{
-        res.status(404).send('Produto não encontrado')
-    }
-
-})
+    res.redirect("/");
+  } else {
+    res.status(404).send("Produto não encontrado");
+  }
+});
